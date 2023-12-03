b14572472e933316b2a30112ed8fc8c9ddec01c2
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-12-04 01:33:16 +0530

    move to separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,21 @@
 import Image from 'next/image'
 import React, { useEffect } from 'react';
 import axios from "axios";
-import {ListSessions} from '@/components/listsessions';
+// import {ListSessions} from '@/components/listsessions';
+const ListLinks = dynamic(() => import('../components/listlinks'), {
+  ssr: false,
+});
 
+import dynamic from 'next/dynamic';
 
 export default function Home() {
   
   const [ss, setss] = React.useState("")
-  const [contents, setcontents] = React.useState(
-    //   [{
-      //   sessionname:"",
-      //   browsername:"",
-      //   tablist:[]
-      // }]
-      []
-      )
+  
       const [showall, setsa] = React.useState(true)
-      const [showdivname,setshowdivname] = React.useState("")
       const [username, setuname] = React.useState("try")
       const [showcreateuser, setcreateuser] = React.useState(false)
-      var data=ListSessions(username);
+      
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
@@ -102,101 +98,7 @@ const addToDb=(event:React.FormEvent)=> {
     console.error(error);
   });
 }
-// const refresh=()=> {
-  // var data:object;
-  // axios.request({
-  //   method: "post",
-  //   url: `https://listallfrompscale.vercel.app/api/user/${username}`
-   
-  // })
-  // .then(response => 
-  //   {
-      // if(response.data)
-      //   {
-          setss("List updated.");
-          // var data=listsessions(username);
-          console.log(typeof(data))
-          // setcontents((response.data))
-          // setuname(uname.value)
-          // setsa(true)
 
-          let tcon=[];
-          let topbar=[];
-          // let children;
-          let persl=[];
-          let ddata=JSON.parse(data.data)
-          console.log(typeof(ddata))
-          ddata.forEach(item => {
-            let children=[];
-            console.log(typeof(item))
-            let titem=JSON.parse(item);
-            console.log(titem)
-            // var children;
-            for(var tinfo in titem.tablist){  
-              // let tinfoeach=JSON.parse(tinfo)
-              // console.log(typeof(tinfo))
-              children.push(
-              <>
-              <tr>
-              {/* <td className='text-clip overflow-hidden'>{titem.tablist[tinfo].url}</td>   */}
-              <td><a href={titem.tablist[tinfo].url}>{titem.tablist[tinfo].title}</a></td>
-              </tr>
-              </>
-              );
-
-            }
-            persl.push(<div className={showdivname==titem.sessionname ? `display-block` : 'hidden'}>{children}</div>)
-
-            topbar.push(<button onClick={()=>{setshowdivname(titem.sessionname);console.log(showdivname)}}>{titem.sessionname}_{titem.browsername}_{titem.tablist.length}</button>);
-            
-            // tcon.props.children.props.children.push(rows);
-            // tcon+=(titem.sessionname)
-            // tcon+="\n"
-            // tcon+=(titem.tablist)
-            
-            // tcon+="\n"
-            // Do something with each item in the array
-          });
-          tcon.push(<>
-              
-            <div >
-            {topbar}
-            <br/>
-            <table className='border-spacing-0 border-gray-100'>
-              {/* <th>
-
-              <td>{titem.sessionname}_{titem.browsername}_{titem.tablist.length}</td>
-              </th> */}
-              {/* <tr> */}
-              {/* <td>test</td> */}
-              {/* </tr> */}
-              {persl}
-            </table>
-            </div>
-            </>);
-          setcontents(tcon)
-      //   }
-      // else
-      //   setss("Failed to add.")
-      // console.log(response.json());
-      
-      // console.log(response)
-  //   })
-  // .catch(error => {
-  //   if (error.response) {
-  //     if(error.response.status==400)
-  //     setss("Issue with server\n"+error.response.status)
-  //     else
-  //     setss("User not found.")
-  //   }
-  //   else
-  //   // Handle any errors
-  //   setss("Issue with server\n"+error)
-  //   console.error(error);
-  // });
-
-  
-// }
 const createUser=(event:React.FormEvent)=> {
   event.preventDefault();
   
@@ -330,8 +232,8 @@ const createUser=(event:React.FormEvent)=> {
   
   {showall?(
 <>
-      <button type="submit" className='h-10 px-6 font-semibold rounded-md bg-blue-600' onClick={refresh}>Refresh</button>
-      <p>{(contents)}</p>
+      
+      <ListLinks username={username}/>
 
 </>
   ):null}
