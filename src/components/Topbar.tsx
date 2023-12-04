630b148482b59593b02bcd77d7a65682aa07182d
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-12-05 01:11:42 +0530

    fix layout to list sessionlist uptop

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -5,38 +5,56 @@ import { ListSessions } from "./listsessions";
 export default function Topbar({username}){
     var {isLoading,isError,data}=ListSessions(username);
     const [showdivname,setshowdivname] = React.useState("")
-
+    let sessionlist=[];
     if(!isLoading && !isError){
 
-        console.log("tdata---------->"+data)
-          let ddata=JSON.parse(data.data)
-          console.log(typeof(ddata))
+        let ddata=JSON.parse((data.data))
+        // let dddata=JSON.parse(ddata);
+        //   console.log(typeof(ddata)) 
+        ddata.map((eitem)=>{
+            let item=JSON.parse(eitem);
+            // console.log("tdata---------->"+(item.sessionname))
+                sessionlist.push({
+                    sname:item.sessionname,
+                    bname:item.browsername,
+                    slength:item.tablist.length
+                });
+          });
           
           return (
           <>
+           {
+           sessionlist.map((slist) => {
+           return (
+           <button onClick={()=>{setshowdivname(slist.sname);console.log(showdivname)}}>{slist.sname}_{slist.bname}_{slist.slength}</button>
+           );
+           })
+           }
           {ddata.map((item) => {
-                    let titem=JSON.parse(item);
+            let titem=JSON.parse(item);
 
-                    return (
-                        <>
-                        <button onClick={()=>{setshowdivname(titem.sessionname);console.log(showdivname)}}>{titem.sessionname}_{titem.browsername}_{titem.tablist.length}</button>
-                        <table className='border-spacing-0 border-gray-100'>
-                        <div className={showdivname==titem.sessionname ? `display-block` : 'hidden'}>
-                            {titem.tablist.map((etab) => {
-                                    return (
-                                        <>
-                                        <tr>
-                                        {/* <td className='text-clip overflow-hidden'>{titem.tablist[tinfo].url}</td>   */}
-                                        <td><a href={etab.url}>{etab.title}</a></td>
-                                        </tr>
-                                        </>
-                                    );
-                                })}
-                        </div>
-                        </table>
-                        </>
-                        );
+            return (
+                <>
+                <table className='border-spacing-0 border-gray-100'>
+                <div 
+                className={showdivname==titem.sessionname ? `display-block` : 'hidden'}
+                >
+                {
+                titem.tablist.map((etab) => {
+                return (
+                    <>
+                    <tr>
+                    {/* <td className='text-clip overflow-hidden'>{titem.tablist[tinfo].url}</td>   */}
+                    <td><a href={etab.url}>{etab.title}</a></td>
+                    </tr>
+                    </>
+                );
                 })}
+                </div>
+                </table>
+                </>
+                );
+            })}
            
           </>
           );
